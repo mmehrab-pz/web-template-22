e792bc84fe94ac61d7cce50a54a7da6cf7fb90b6
Mehrab <mmehrab.pk@gmail.com>
2025-12-11 15:43:55 +0330

    PageTitle component added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Header from '../../components/layout/Header'
+import Footer from "@/components/layout/Footer";
+import TrustedTeams from "@/components/layout/TrustedTeams";
+import CustomerStories from "@/components/layout/CustomerStories";
+import FAQ from "@/components/layout/FAQ";
+import PageTitle from "@/components/layout/PageTitle";
 
 
 export default function page() {
   return (
     <>
       <Header />
-      <h2>about</h2>
+      <PageTitle
+      miniTitle='About AIMug'
+      title='A Magical Tool For Seamless Collaboration'
+      content='Mastering the Art of AI Tool: Unleashing the Power of Automated Creativity with AIMug"'
+      />
+      <TrustedTeams />
+      <CustomerStories />
+      <FAQ />
+      <Footer />
     </>
   );
 }
