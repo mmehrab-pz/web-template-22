84aafeb2684073b8e3f3c2468d1a992eaa801c2a
Mehrab <mmehrab.pk@gmail.com>
2025-12-11 23:32:38 +0330

    contact page started

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,22 @@
+// ----------------------contact page------------------
 import React from "react";
 import Header from '../../components/layout/Header'
+import Footer from "@/components/layout/Footer";
+import FAQ from "@/components/layout/FAQ";
+import PageTitle from "@/components/layout/PageTitle";
 
 
 export default function page() {
   return (
     <>
       <Header />
-      <h2>contact</h2>
+      <PageTitle 
+      miniTitle='Contact Us'
+      title='Our Dynamic Team are Waiting for Reach you'
+      content='Mastering the Art of AI Tool: Unleashing the Power of Automated Creativity with AIMug"'
+      />
+      <FAQ />
+      <Footer />
     </>
   );
 }
